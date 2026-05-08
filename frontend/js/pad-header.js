cafe347ed978142df7c52d7c1cfca4218ab2e510
velocity6097dev <velocity6097.dev@gmail.com>
2026-05-08 19:34:32 +0530

    header logo pad change

--- frontend/js/pad-header.js
+++ frontend/js/pad-header.js
@@ -61,7 +61,7 @@ function injectBaharHeader() {
 
         headerContainer.innerHTML = `
             <div style="display: flex; align-items: center; justify-content: center; position: relative; padding-bottom: 1rem; font-family: 'Poppins', sans-serif;">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Official_BPCL_LOGO.jpg" alt="BPCL" style="position: absolute; left: 0; top: 50%; transform: translateY(-50%); width: 80px; height: auto;">
+                <img src="https://files.catbox.moe/vntcbm.jpg" alt="BPCL" style="position: absolute; left: 0; top: 50%; transform: translateY(-50%); width: 80px; height: auto;">
                 <div style="text-align: center; width: 100%; padding: 0 90px;">
                     <h1 style="margin:0; color: #004b87; font-size: 1.6rem; font-weight: 800; letter-spacing: 1px;">M/S. BAHAR SERVICE STATION</h1>
                     <p style="margin:4px 0 2px 0; font-weight: 700; font-size: 1rem; color: black;">DEALER: BHARAT PETROLEUM CORP LTD.</p>
